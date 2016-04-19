6625ceb6eb049ee159b4ef8bf0218e17f69802db
boss <boss@FK1>
2016-04-19 12:41:05 +0200

    Welcome middleware with Microsoft.Aspnet.Diagnostics package

--- src/OdeToFood/Startup.cs
+++ src/OdeToFood/Startup.cs
@@ -26,12 +26,15 @@ namespace OdeToFood
             services.AddSingleton<IGreeter, Greeder>();
         }
 
-        // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
+        // This method gets called by the runtime. 
+        //Use this method to configure the HTTP request pipeline.
         public void Configure(
             IApplicationBuilder app,
             IGreeter greeter)
         {
             app.UseIISPlatformHandler();
+
+            app.UseWelcomePage();
 
             app.Run(async (context) =>
             {
