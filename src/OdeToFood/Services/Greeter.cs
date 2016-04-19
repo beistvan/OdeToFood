84b45fe400fd505c0a252ddb7575d71de98bf97b
boss <boss@FK1>
2016-04-19 12:01:15 +0200

    Greeter from the service

---
+++ src/OdeToFood/Services/Greeter.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace OdeToFood.Services
+{
+    public interface IGreeter
+    {
+        string GetGreeting();
+    }
+
+    public class Greeder : IGreeter
+    {
+        public string GetGreeting()
+        {
+            return "Hello from the greeter!";
+        }
+    }
+}
