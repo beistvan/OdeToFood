e80d7987264eb5fa4df5afa2d0c2c956bda49e68
boss <boss@FK1>
2016-04-19 12:11:08 +0200

    Configuration from service

--- src/OdeToFood/Services/Greeter.cs
+++ src/OdeToFood/Services/Greeter.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using Microsoft.Extensions.Configuration;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
@@ -12,9 +13,16 @@ namespace OdeToFood.Services
 
     public class Greeder : IGreeter
     {
+        private string _greeting;
+
+        public Greeder(IConfiguration configuration)
+        {
+            _greeting = configuration["greeting"];
+        }
+
         public string GetGreeting()
         {
-            return "Hello from the greeter!";
+            return _greeting;
         }
     }
 }
