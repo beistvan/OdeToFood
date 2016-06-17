f7f15c0404ab02e316b55bac6f9018cdb98d662a
boss <boss@FK1>
2016-06-17 23:28:07 +0200

    Implementing DbContext with DbSet

---
+++ src/OdeToFood/Entities/OdeToFoodDbContext.cs
@@ -0,0 +1,13 @@
+﻿using Microsoft.Data.Entity;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace OdeToFood.Entities
+{
+    public class OdeToFoodDbContext : DbContext
+    {   
+        public DbSet<Restaurant> Restaurants { get; set; }
+    }
+}
