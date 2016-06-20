5164e9fe0bbeb6539cd1c7627fabf87571205e02
boss <boss@FK1>
2016-06-20 12:20:14 +0200

    Log In

--- src/OdeToFood/Controllers/AccountController.cs
+++ src/OdeToFood/Controllers/AccountController.cs
@@ -49,6 +49,36 @@ namespace OdeToFood.Controllers
             return View();
         }
 
+        [HttpGet]
+        public IActionResult Login(string returnUrl = "")
+        {
+            var model = new LoginViewModel { ReturnUrl = returnUrl };
+            return View(model);
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> Login(LoginViewModel model)
+        {
+            if (ModelState.IsValid) {
+                var result = await _signInManager.PasswordSignInAsync(
+                    model.Username, model.Password, model.RememberMe, false);
+                if (result.Succeeded)
+                {
+                    if (!string.IsNullOrEmpty(model.ReturnUrl) &&
+                            Url.IsLocalUrl(model.ReturnUrl))
+                    {
+                        return Redirect(model.ReturnUrl);
+                    }
+                    else
+                    {
+                        return RedirectToAction("Index", "Home");
+                    }
+                }
+            }
+            ModelState.AddModelError("", "Invalid login attempt");
+            return View(model);
+        }
+
         [HttpPost]
         public async Task<IActionResult> Logout()
         {
---
+++ src/OdeToFood/ViewModels/LoginViewModel.cs
@@ -0,0 +1,14 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace OdeToFood.ViewModels
+{
+    public class LoginViewModel
+    {
+        public string Username { get; set; }
+        [DataType(DataType.Password)]
+        public string Password { get; set; }
+        [Display(Name = "Remenber Me")]
+        public bool RememberMe { get; set; }
+        public string ReturnUrl { get; set; }
+    }
+}
